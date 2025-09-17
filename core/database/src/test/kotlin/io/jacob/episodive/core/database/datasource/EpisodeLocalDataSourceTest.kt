36f2478900f9aa3919d3697d71e293d19937e580
Jacob Kang <jacob@litbig.com>
2025-09-17 17:26:17 +0900

    test: database dataSource unitTest

---
+++ core/database/src/test/kotlin/io/jacob/episodive/core/database/datasource/EpisodeLocalDataSourceTest.kt
@@ -0,0 +1,287 @@
+package io.jacob.episodive.core.database.datasource
+
+import androidx.room.withTransaction
+import io.jacob.episodive.core.database.EpisodiveDatabase
+import io.jacob.episodive.core.database.dao.EpisodeDao
+import io.jacob.episodive.core.database.mapper.toEpisodeEntities
+import io.jacob.episodive.core.database.mapper.toEpisodeEntity
+import io.jacob.episodive.core.testing.data.episodeTestData
+import io.jacob.episodive.core.testing.data.episodeTestDataList
+import io.jacob.episodive.core.testing.util.MainDispatcherRule
+import io.mockk.Runs
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.coVerifySequence
+import io.mockk.confirmVerified
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.mockkStatic
+import io.mockk.slot
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import org.junit.Rule
+import org.junit.Test
+
+class EpisodeLocalDataSourceTest {
+    @get:Rule
+    val mainDispatcherRule = MainDispatcherRule()
+
+    private val database = mockk<EpisodiveDatabase>(relaxed = true)
+    private val episodeDao = mockk<EpisodeDao>(relaxed = true)
+
+    private val dataSource: EpisodeLocalDataSource =
+        EpisodeLocalDataSourceImpl(
+            database = database,
+            episodeDao = episodeDao,
+        )
+
+    private val episodeEntity = episodeTestData.toEpisodeEntity()
+    private val episodeEntities = episodeTestDataList.toEpisodeEntities()
+
+    @Test
+    fun `Given dependencies, When upsertEpisode is called, Then upsertEpisode of dao is called`() =
+        runTest {
+            // Given
+            coEvery { episodeDao.upsertEpisode(any()) } just Runs
+
+            // When
+            dataSource.upsertEpisode(episodeEntity)
+
+            // Then
+            coVerify { episodeDao.upsertEpisode(episodeEntity) }
+            confirmVerified(
+                database,
+                episodeDao,
+            )
+        }
+
+    @Test
+    fun `Given dependencies, When upsertEpisodes is called, Then upsertEpisodes of dao is called`() =
+        runTest {
+            // Given
+            coEvery { episodeDao.upsertEpisodes(any()) } just Runs
+
+            // When
+            dataSource.upsertEpisodes(episodeEntities)
+
+            // Then
+            coVerify { episodeDao.upsertEpisodes(episodeEntities) }
+            confirmVerified(
+                database,
+                episodeDao,
+            )
+        }
+
+    @Test
+    fun `Given dependencies, When getEpisode is called, Then getEpisode of dao is called`() =
+        runTest {
+            // Given
+            coEvery { episodeDao.getEpisode(any()) } returns mockk()
+
+            // When
+            dataSource.getEpisode(episodeEntity.id)
+
+            // Then
+            coVerify { episodeDao.getEpisode(episodeEntity.id) }
+            confirmVerified(
+                database,
+                episodeDao,
+            )
+        }
+
+    @Test
+    fun `Given dependencies, When getEpisodes is called, Then getEpisodes of dao is called`() =
+        runTest {
+            // Given
+            coEvery { episodeDao.getEpisodes() } returns mockk()
+
+            // When
+            dataSource.getEpisodes()
+
+            // Then
+            coVerify { episodeDao.getEpisodes() }
+            confirmVerified(
+                database,
+                episodeDao,
+            )
+        }
+
+    @Test
+    fun `Given dependencies, When getEpisodesPaging is called, Then getEpisodesPaging of dao is called`() =
+        runTest {
+            // Given
+            coEvery { episodeDao.getEpisodesPaging() } returns mockk()
+
+            // When
+            dataSource.getEpisodesPaging()
+
+            // Then
+            coVerify { episodeDao.getEpisodesPaging() }
+            confirmVerified(
+                database,
+                episodeDao,
+            )
+        }
+
+    @Test
+    fun `Given dependencies, When getLikedEpisodes is called, Then getLikedEpisodes of dao is called`() =
+        runTest {
+            // Given
+            coEvery { episodeDao.getLikedEpisodes() } returns mockk()
+
+            // When
+            dataSource.getLikedEpisodes()
+
+            // Then
+            coVerify { episodeDao.getLikedEpisodes() }
+            confirmVerified(
+                database,
+                episodeDao,
+            )
+        }
+
+    @Test
+    fun `Given dependencies, When isLiked is called, Then isLiked of dao is called`() =
+        runTest {
+            // Given
+            coEvery { episodeDao.isLiked(any()) } returns mockk()
+
+            // When
+            dataSource.isLiked(episodeEntity.id)
+
+            // Then
+            coVerify { episodeDao.isLiked(episodeEntity.id) }
+            confirmVerified(
+                database,
+                episodeDao,
+            )
+        }
+
+    @Test
+    fun `Given isLiked return false, When toggleLike is called, Then addLike is called`() =
+        runTest {
+            // Given
+            mockkStatic("androidx.room.RoomDatabaseKt")
+            val transactionSlot = slot<suspend () -> Unit>()
+            coEvery { episodeDao.isLiked(any()) } returns flowOf(false)
+            coEvery {
+                database.withTransaction(capture(transactionSlot))
+            } coAnswers {
+                transactionSlot.captured.invoke()
+            }
+            coEvery { episodeDao.addLike(any()) } just Runs
+            coEvery { episodeDao.removeLike(any()) } just Runs
+
+            // When
+            dataSource.toggleLike(episodeEntity.id)
+
+            // Then
+            coVerifySequence {
+                episodeDao.isLiked(episodeEntity.id)
+                database.withTransaction(any<suspend () -> Unit>())
+                episodeDao.addLike(any())
+            }
+            confirmVerified(
+                database,
+                episodeDao,
+            )
+        }
+
+    @Test
+    fun `Given isLiked return true, When toggleLike is called, Then removeLike is called`() =
+        runTest {
+            // Given
+            mockkStatic("androidx.room.RoomDatabaseKt")
+            val transactionSlot = slot<suspend () -> Unit>()
+            coEvery { episodeDao.isLiked(any()) } returns flowOf(true)
+            coEvery {
+                database.withTransaction(capture(transactionSlot))
+            } coAnswers {
+                transactionSlot.captured.invoke()
+            }
+            coEvery { episodeDao.addLike(any()) } just Runs
+            coEvery { episodeDao.removeLike(any()) } just Runs
+
+            // When
+            dataSource.toggleLike(episodeEntity.id)
+
+            // Then
+            coVerifySequence {
+                episodeDao.isLiked(episodeEntity.id)
+                database.withTransaction(any<suspend () -> Unit>())
+                episodeDao.removeLike(episodeEntity.id)
+            }
+            confirmVerified(
+                database,
+                episodeDao,
+            )
+        }
+
+    @Test
+    fun `Given dependencies, When deleteEpisode is called, Then deleteEpisode of dao is called`() =
+        runTest {
+            // Given
+            coEvery { episodeDao.deleteEpisode(any()) } just Runs
+
+            // When
+            dataSource.deleteEpisode(episodeEntity.id)
+
+            // Then
+            coVerify { episodeDao.deleteEpisode(episodeEntity.id) }
+            confirmVerified(
+                database,
+                episodeDao,
+            )
+        }
+
+    @Test
+    fun `Given dependencies, When deleteEpisodes is called, Then deleteEpisodes of dao is called`() =
+        runTest {
+            // Given
+            coEvery { episodeDao.deleteEpisodes() } just Runs
+
+            // When
+            dataSource.deleteEpisodes()
+
+            // Then
+            coVerify { episodeDao.deleteEpisodes() }
+            confirmVerified(
+                database,
+                episodeDao,
+            )
+        }
+
+    @Test
+    fun `Given dependencies, When getEpisodeCount is called, Then getEpisodeCount of dao is called`() =
+        runTest {
+            // Given
+            coEvery { episodeDao.getEpisodeCount() } returns mockk()
+
+            // When
+            dataSource.getEpisodeCount()
+
+            // Then
+            coVerify { episodeDao.getEpisodeCount() }
+            confirmVerified(
+                database,
+                episodeDao,
+            )
+        }
+
+    @Test
+    fun `Given dependencies, When getLikedEpisodeCount is called, Then getLikedEpisodeCount of dao is called`() =
+        runTest {
+            // Given
+            coEvery { episodeDao.getLikedEpisodeCount() } returns mockk()
+
+            // When
+            dataSource.getLikedEpisodeCount()
+
+            // Then
+            coVerify { episodeDao.getLikedEpisodeCount() }
+            confirmVerified(
+                database,
+                episodeDao,
+            )
+        }
+}
---
+++ core/database/src/test/kotlin/io/jacob/episodive/core/database/datasource/PodcastLocalDataSourceTest.kt
@@ -0,0 +1,144 @@
+package io.jacob.episodive.core.database.datasource
+
+import io.jacob.episodive.core.database.dao.PodcastDao
+import io.jacob.episodive.core.database.mapper.toPodcastEntities
+import io.jacob.episodive.core.database.mapper.toPodcastEntity
+import io.jacob.episodive.core.testing.data.podcastTestData
+import io.jacob.episodive.core.testing.data.podcastTestDataList
+import io.jacob.episodive.core.testing.util.MainDispatcherRule
+import io.mockk.Runs
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.confirmVerified
+import io.mockk.just
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.Rule
+import org.junit.Test
+
+class PodcastLocalDataSourceTest {
+    @get:Rule
+    val mainDispatcherRule = MainDispatcherRule()
+
+    private val podcastDao = mockk<PodcastDao>(relaxed = true)
+
+    private val dataSource: PodcastLocalDataSource =
+        PodcastLocalDataSourceImpl(
+            podcastDao = podcastDao,
+        )
+
+    private val podcastEntity = podcastTestData.toPodcastEntity()
+    private val podcastEntities = podcastTestDataList.toPodcastEntities()
+
+    @Test
+    fun `Given dependencies, When upsertPodcast is called, Then upsertPodcast of dao is called`() =
+        runTest {
+            // Given
+            coEvery { podcastDao.upsertPodcast(any()) } just Runs
+
+            // When
+            dataSource.upsertPodcast(podcastEntity)
+
+            // Then
+            coVerify { podcastDao.upsertPodcast(podcastEntity) }
+            confirmVerified(
+                podcastDao,
+            )
+        }
+
+    @Test
+    fun `Given dependencies, When upsertPodcasts is called, Then upsertPodcasts of dao is called`() =
+        runTest {
+            // Given
+            coEvery { podcastDao.upsertPodcasts(any()) } just Runs
+
+            // When
+            dataSource.upsertPodcasts(podcastEntities)
+
+            // Then
+            coVerify { podcastDao.upsertPodcasts(podcastEntities) }
+            confirmVerified(
+                podcastDao,
+            )
+        }
+
+    @Test
+    fun `Given dependencies, When getPodcast is called, Then getPodcast of dao is called`() =
+        runTest {
+            // Given
+            coEvery { podcastDao.getPodcast(any()) } returns mockk()
+
+            // When
+            dataSource.getPodcast(podcastEntity.id)
+
+            // Then
+            coVerify { podcastDao.getPodcast(podcastEntity.id) }
+            confirmVerified(
+                podcastDao,
+            )
+        }
+
+    @Test
+    fun `Given dependencies, When getPodcasts is called, Then getPodcasts of dao is called`() =
+        runTest {
+            // Given
+            coEvery { podcastDao.getPodcasts() } returns mockk()
+
+            // When
+            dataSource.getPodcasts()
+
+            // Then
+            coVerify { podcastDao.getPodcasts() }
+            confirmVerified(
+                podcastDao,
+            )
+        }
+
+    @Test
+    fun `Given dependencies, When getPodcastsPaging is called, Then getPodcastsPaging of dao is called`() =
+        runTest {
+            // Given
+            coEvery { podcastDao.getPodcastsPaging() } returns mockk()
+
+            // When
+            dataSource.getPodcastsPaging()
+
+            // Then
+            coVerify { podcastDao.getPodcastsPaging() }
+            confirmVerified(
+                podcastDao,
+            )
+        }
+
+    @Test
+    fun `Given dependencies, When deletePodcast is called, Then deletePodcast of dao is called`() =
+        runTest {
+            // Given
+            coEvery { podcastDao.deletePodcast(any()) } just Runs
+
+            // When
+            dataSource.deletePodcast(podcastEntity.id)
+
+            // Then
+            coVerify { podcastDao.deletePodcast(podcastEntity.id) }
+            confirmVerified(
+                podcastDao,
+            )
+        }
+
+    @Test
+    fun `Given dependencies, When deletePodcasts is called, Then deletePodcasts of dao is called`() =
+        runTest {
+            // Given
+            coEvery { podcastDao.deletePodcasts() } just Runs
+
+            // When
+            dataSource.deletePodcasts()
+
+            // Then
+            coVerify { podcastDao.deletePodcasts() }
+            confirmVerified(
+                podcastDao,
+            )
+        }
+}
