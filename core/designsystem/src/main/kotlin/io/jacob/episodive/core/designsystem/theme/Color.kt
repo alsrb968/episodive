008b0b40375711c228794ea261f3668f8222f901
Jacob Kang <jacob@litbig.com>
2025-09-24 00:26:54 +0900

    feat: DesignSystem font, color

---
+++ core/designsystem/src/main/kotlin/io/jacob/episodive/core/designsystem/theme/Color.kt
@@ -0,0 +1,96 @@
+package io.jacob.episodive.core.designsystem.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Red Primary - #F5332C 기반
+val primaryLight = Color(0xFFF5332C)
+val onPrimaryLight = Color(0xFFFFFFFF)
+val primaryContainerLight = Color(0xFFFFDAD6)
+val onPrimaryContainerLight = Color(0xFF410002)
+
+// Secondary - Red의 변형
+val secondaryLight = Color(0xFFD63C2F)
+val onSecondaryLight = Color(0xFFFFFFFF)
+val secondaryContainerLight = Color(0xFFFFE0DE)
+val onSecondaryContainerLight = Color(0xFF370B09)
+
+// Tertiary - Red-Orange 보완색
+val tertiaryLight = Color(0xFFE65100)
+val onTertiaryLight = Color(0xFFFFFFFF)
+val tertiaryContainerLight = Color(0xFFFFDBCC)
+val onTertiaryContainerLight = Color(0xFF2D0900)
+
+val errorLight = Color(0xFFBA1A1A)
+val onErrorLight = Color(0xFFFFFFFF)
+val errorContainerLight = Color(0xFFFFDAD6)
+val onErrorContainerLight = Color(0xFF410002)
+
+// Neutral - Light Background 계열
+val backgroundLight = Color(0xFFFFFBFF)
+val onBackgroundLight = Color(0xFF201A1A)
+val surfaceLight = Color(0xFFFFFBFF)
+val onSurfaceLight = Color(0xFF201A1A)
+val surfaceVariantLight = Color(0xFFF5DDDA)
+val onSurfaceVariantLight = Color(0xFF534341)
+
+val outlineLight = Color(0xFF857370)
+val outlineVariantLight = Color(0xFFD8C2BE)
+val scrimLight = Color(0xFF000000)
+val inverseSurfaceLight = Color(0xFF362F2F)
+val inverseOnSurfaceLight = Color(0xFFFBEEEE)
+val inversePrimaryLight = Color(0xFFFFB3A6)
+
+val surfaceDimLight = Color(0xFFE8D6D5)
+val surfaceBrightLight = Color(0xFFFFFBFF)
+val surfaceContainerLowestLight = Color(0xFFFFFFFF)
+val surfaceContainerLowLight = Color(0xFFFFF0EF)
+val surfaceContainerLight = Color(0xFFF9EAEA)
+val surfaceContainerHighLight = Color(0xFFF3E4E4)
+val surfaceContainerHighestLight = Color(0xFFEDDEDE)
+
+
+// Red Primary - #F5332C 기반
+val primaryDark = Color(0xFFF5332C)
+val onPrimaryDark = Color(0xFFFFFFFF)
+val primaryContainerDark = Color(0xFF8B1E1A)
+val onPrimaryContainerDark = Color(0xFFFFDAD6)
+
+// Secondary - Red의 변형
+val secondaryDark = Color(0xFFFF5449)
+val onSecondaryDark = Color(0xFF000000)
+val secondaryContainerDark = Color(0xFF9C2B23)
+val onSecondaryContainerDark = Color(0xFFFFE0DE)
+
+// Tertiary - Red-Orange 보완색
+val tertiaryDark = Color(0xFFFF7043)
+val onTertiaryDark = Color(0xFF2D0900)
+val tertiaryContainerDark = Color(0xFF5C1A00)
+val onTertiaryContainerDark = Color(0xFFFFDBCC)
+
+val errorDark = Color(0xFFFF6B6B)
+val onErrorDark = Color(0xFF370001)
+val errorContainerDark = Color(0xFF5F1213)
+val onErrorContainerDark = Color(0xFFFFDAD6)
+
+// Neutral - Dark Background 계열
+val backgroundDark = Color(0xFF1A1414)
+val onBackgroundDark = Color(0xFFFFFFFF)
+val surfaceDark = Color(0xFF1A1414)
+val onSurfaceDark = Color(0xFFFFFFFF)
+val surfaceVariantDark = Color(0xFF2A2424)
+val onSurfaceVariantDark = Color(0xFFB3ADAD)
+
+val outlineDark = Color(0xFF535050)
+val outlineVariantDark = Color(0xFF2A2424)
+val scrimDark = Color(0xFF000000)
+val inverseSurfaceDark = Color(0xFFFFFFFF)
+val inverseOnSurfaceDark = Color(0xFF1A1414)
+val inversePrimaryDark = Color(0xFF8B1E1A)
+
+val surfaceDimDark = Color(0xFF0D0B0B)
+val surfaceBrightDark = Color(0xFF2A2626)
+val surfaceContainerLowestDark = Color(0xFF0A0808)
+val surfaceContainerLowDark = Color(0xFF121010)
+val surfaceContainerDark = Color(0xFF1A1616)
+val surfaceContainerHighDark = Color(0xFF212020)
+val surfaceContainerHighestDark = Color(0xFF2A2828)
